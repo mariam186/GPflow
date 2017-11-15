d97685bc743fe8a702359eb8f6a77e60e53155b1
ST John <st@prowler.io>
2017-11-15 11:35:11 +0000

    explicit kwargs for conditionals

--- gpflow/features.py
+++ gpflow/features.py
@@ -140,7 +140,7 @@ class Multiscale(InducingPoints):
 
 
 @singledispatch
-def conditional(feat, kern, Xnew, f, full_cov=False, q_sqrt=None, whiten=False):
+def conditional(feat, kern, Xnew, f, *, full_cov=False, q_sqrt=None, whiten=False):
     """
     Note the changed function signature compared to conditionals.conditional()
     to allow for single dispatch on the first argument.
@@ -150,7 +150,7 @@ def conditional(feat, kern, Xnew, f, full_cov=False, q_sqrt=None, whiten=False):
 
 @conditional.register(InducingPoints)
 @conditional.register(Multiscale)
-def default_feature_conditional(feat, kern, Xnew, f, full_cov=False, q_sqrt=None, whiten=False):
+def default_feature_conditional(feat, kern, Xnew, f, *, full_cov=False, q_sqrt=None, whiten=False):
     """
     Uses the same code path as conditionals.conditional(), except Kuu/Kuf
     matrices are constructed using the feature.
